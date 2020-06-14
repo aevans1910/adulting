c04491e519060c3992ba9aeadb2336abf4233334
Ariane Evans <ariane.evans@student.makeschool.com>
2020-06-14 13:38:26 -0700

    [add] styling

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 export default function SignInForm() {
     return (
-        <div className="sign-in">
-            <h1>Sign in</h1>
+        <div className="sign-up-in">
+            <div>
+                <button>Sign In</button>
+            </div>
             <div className="form">
                 <p name="email">Email</p>
-                <input type="email" name="email" id="email"/>
+                <input type="email" name="email" id="email" placeholder="Email"/>
                 <p name="password">Password</p>
-                <input type="password" name="password" id="password"/>
+                <input type="password" name="password" id="password" placeholder="Password"/>
+                <button type="submit">SIGN IN</button>
             </div>
         </div>
     )
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function SignUpForm() {
     return (
-        <div className="sign-up">
-            <h1>Sign up</h1>
+        <div className="sign-up-in">
+            <div>
+                <button>Sign Up</button>
+            </div>
             <div className="form">
                 <p name="email">Email</p>
                 <input type="email" name="email" id="email" placeholder="Email"/>
@@ -11,7 +13,7 @@ export default function SignUpForm() {
                 <input type="password" name="password" id="password" placeholder="Password"/>
                 <p name="re-password">Re-enter password</p>
                 <input type="password" name="re-password" id="re-password" placeholder="Password"/>
-                <button type="submit">Sign up</button>
+                <button type="submit">SIGN UP</button>
             </div>
         </div>
     )
