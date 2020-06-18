05d270f351394f505506110ea7ddd72892187cfb
donald vallejo <donny.vallejo@gmail.com>
2020-06-18 00:27:32 -0700

    added a model

---
+++ models/post.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  summary: { type: String, required: true }
+});
+
+module.exports = mongoose.model("Post", PostSchema);
