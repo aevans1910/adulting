b5c7bae7f6796418d4daaee8751eed0e7356d7cb
Ian Rones <ronesp.85@gmail.com>
2020-06-19 10:23:32 -0700

    [edit] class changes to sign in and sign up form

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function SignInForm() {
+export default class SignInForm() extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    };
+    handleChange(evt){
+        this.setState({[evt.target.name] : evt.target.value})
+    }
+    handleSubmit(evt){
+        evt.preventDefault();
+        this.setState({ username: "", email: "", password: ""})
+    }
     return (
-        <div className="sign-up-in">
-            <div className="form">
-                <p name="email">Email</p>
-                <input type="email" name="email" id="email" placeholder="Email"/>
-                <p name="password">Password</p>
-                <input type="password" name="password" id="password" placeholder="Password"/>
+        <div className="Sign-In">
+            <div className="Form">
+                <input
+                    type="text"
+                    name="username"
+                    id="id"
+                    placeholder="Username"
+                    value={this.state.username}
+                    onChange={ this.handleChange }
+                 />
+                <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    placeholder="Email Address"
+                    value={this.state.email} 
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    />
                 <button type="submit">SIGN IN</button>
             </div>
         </div>
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,17 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function SignUpForm() {
-    return (
-        <div className="sign-up-in">
-            <div className="form">
-                <p name="email">Email</p>
-                <input type="email" name="email" id="email" placeholder="Email"/>
-                <p name="password">Password</p>
-                <input type="password" name="password" id="password" placeholder="Password"/>
-                <p name="re-password">Re-enter password</p>
-                <input type="password" name="re-password" id="re-password" placeholder="Password"/>
-                <button type="submit">SIGN UP</button>
+export default class SignupForm extends Component {
+    constructor(props){
+        super(props)={
+            username: "",
+            email: "",
+            password: "",
+            re_password: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    };
+    handleChange(evt){
+        this.setState({[evt.target.name] : evt.target.value})
+    }
+    handleSubmit(evt){
+        evt.preventDefault();
+        this.setState({ username: "", email: "",password: "", re_password:""});
+    }
+    render(){
+        return(
+            <div className="Sign-Up">
+                <form className="Form">
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="username"
+                        value={this.state.username}
+                        onChange= { this.handleChange }
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="email address"
+                        value={ this.state.username }
+                        onChange={ this.handleChange }
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={ this.state.password }
+                        onChange={ this.handleChange }
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="re_password"
+                        placeholder="Re-enter your password"
+                        value={ this.state.re_password }
+                        onChange = { this.handleChange }
+                     />
+
+                     <button>Let's Go!</button>
+                </form>
             </div>
-        </div>
-    )
+        )
+    }
 }
