028b270d9077c6d2f719f929714607b7bf03f274
donald vallejo <donny.vallejo@gmail.com>
2020-06-18 22:49:35 -0700

    imporved models for comments and user

--- models/post.js
+++ models/post.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
-  summary: { type: String, required: true }
+  summary: { type: String, required: true },
+  subreddit: { type: String, required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  author : { type: Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 module.exports = mongoose.model("Post", PostSchema);
