3ac4804a994dbdd48130161905d6e9827f17c735
donald vallejo <donny.vallejo@gmail.com>
2020-06-21 22:56:59 -0700

    added front end to back end

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
 const express = require('express');
-const app = express();
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const handlebars = require('handlebars');
+
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+});
+
+require("dotenv").config()
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const expressValidator = require('express-validator');
+// const app = express();
+var app = express();
 const port = 3000
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+      req.user = null;
+    } else {
+      var token = req.cookies.nToken;
+      var decodedToken = jwt.decode(token, { complete: true }) || {};
+      req.user = decodedToken.payload;
+    }
+  
+    next();
+  };
+
+app.use(cookieParser())
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+app.use(checkAuth);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(express.static('public'));
+require('./data/reddit-db');
+require('./controllers/posts.js')(app);
+require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 
-app.get('/', (req, res) => {
-    res.render('home')
-});
-
-// POST method route
-app.get('/posts/new', (req, res) => {
-    res.render('posts-new')
-});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+module.exports = app;
