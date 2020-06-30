b418d157bcd32cbece5906c03437c9d63c3a851c
donald vallejo <donny.vallejo@gmail.com>
2020-06-30 13:35:43 -0700

    converted routes to work with react KI

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,15 +1,39 @@
 import React from 'react'
 
-export default function SignInForm() {
-    return (
-        <div className="sign-up-in">
-            <div className="form">
-                <p name="email">Email</p>
-                <input type="email" name="email" id="email" placeholder="Email"/>
-                <p name="password">Password</p>
-                <input type="password" name="password" id="password" placeholder="Password"/>
-                <button type="submit">SIGN IN</button>
+export default class SignInForm {
+    constructor(props){
+        this.state = {
+            email: "",
+            password: "",
+        }
+    }
+    submit(){
+        fetch("https://localhost:3050/log-in", {
+            method:"POST",
+            mode:"cors",
+            headers:{"Content-Type" : "application/json"},
+            body: JSON.stringify(this.state)
+        })
+        .then(res => res.json()).then(data => {
+            console.log(data)
+            // TODO: save the token to local storage
+            // TODO: redirect to home page
+            // TODO: use react router dom
+        })
+    }
+    render(){
+        const {email, password} = this.state
+        return (
+            <div className="sign-up-in">
+                <div className="form">
+                    <p name="email">Email</p>
+                    <input value={email} onChange={(e) => this.setState({email:e.target.value})} type="email" name="email" id="email" placeholder="Email"/>
+
+                    <p name="password">Password</p>
+                    <input value={password} onChange={(e) => this.setState({password:e.target.value})} type="password" name="password" id="password" placeholder="Password"/>
+                    <button onClick={()=>this.submit()} type="submit">SIGN IN</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
