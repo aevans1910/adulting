7d9caf1f0ac288ea091fe3fe221c4d6cba85f738
donald vallejo <donny.vallejo@gmail.com>
2020-07-08 11:30:11 -0700

    outs still aren't working

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ import './styles/pages.css'
 import SignInForm from './components/SignInForm';
 import SignUpForm from './components/SignUpForm';
 
-const Temp = () => return(
+const Temp = () => {
+  return(
   <div>
     HOME
     <Link to="/sign-up">Sign-Up</Link>
     <Link to="/sign-in">Sign-In</Link>
     <button onClick={()=>this.submit()} type="submit">SIGN UP</button>
   </div>
-);
+  )
+};
 
 function App() {
   return (
@@ -25,7 +27,7 @@ function App() {
         <Route path='/' exact component={Temp} />
         <Route path='/sign-up' component={SignUpForm} />
         <Route path='/sign-in' component={SignInForm} />
-        <Route path='/log-out' component={}/>
+        {/* <Route path='/log-out' component={}/> */}
         <Route render={routeProps => <p>404</p>} />
       </Switch>
     </Router>
