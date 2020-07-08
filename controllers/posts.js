0322a60ffff3d7b51e7172bb6f6b253c60ce6eee
donald vallejo <donny.vallejo@gmail.com>
2020-07-07 20:26:23 -0700

    outs still aren't working

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,29 +9,30 @@ module.exports = (app) => {
 
   // CREATE
   app.post("/post/new", (req, res) => {
-        const userId = req.user._id
-        User.findById(userId).then(user => {
-            if (user) {
-                var post = new Post(req.body);
-                post.author = user._id;
-                post.save()
-                    .then(post => {
-                        user.posts.unshift(post);
-                        user.save();
-                        // REDIRECT TO THE NEW POST
-                        res.status(200).json({postId : post._id});
-                    })
-                    .catch(err => {
-                        console.log(err.message);
-                    });
-            } else {
-                return res.status(401); // UNAUTHORIZED
-            }
-        })
+    const userId = req.user._id
+    User.findById(userId).then(user => {
+        if (user) {
+            var post = new Post(req.body);
+            post.author = user._id;
+            post.save()
+                .then(post => {
+                    user.posts.unshift(post);
+                    user.save();
+                    // REDIRECT TO THE NEW POST
+                    res.status(200).json({postId : post._id});
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
+        } else {
+            return res.status(401); // UNAUTHORIZED
+        }
+    })
 });
   
   // SHOW
 app.get("/posts/:id", function (req, res) {
+    const userId = req.user._id
     Post.findById(req.params.id).populate({path:'comments', populate: {path: 'author'}}).populate('author')
         .then(post => {
             res.json({ post });  
@@ -55,6 +56,7 @@ app.get("/posts/:id", function (req, res) {
   
   // SUBREDDIT
 app.get("/n/:subreddit", function (req, res) {
+    const userId = req.user._id
   Post.find({ subreddit: req.params.subreddit }).populate('author')
       .then(posts => {
           res.render("posts-index", { posts });
