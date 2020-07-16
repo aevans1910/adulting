6d36af0debc7b7ff054d96512ec9b9d216dea482
donald vallejo <donny.vallejo@gmail.com>
2020-07-15 20:23:26 -0700

    removing files for push from final branch

--- back-end/models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const CommentSchema = new Schema({
-  content: { type: String, required: true },
-  author : { type: Schema.Types.ObjectId, ref: "User", required: true }
-});
-
-module.exports = mongoose.model("Comment", CommentSchema);
